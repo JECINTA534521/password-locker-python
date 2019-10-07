b5b99f180beceee8cd422446c973a2922f3aef34
jecinta <jecintawanjirug@gmail.com>
2019-10-07 10:51:53 +0300

    this imports the random and string method

--- user_credentials.py
+++ user_credentials.py
@@ -2,11 +2,11 @@ import pyperclip
 import random
 import string
 
-global users_list 
+global users_list
 class User:
-	'''
-	Class to create user accounts and save their information
-	'''
+    '''
+    Class to create user accounts and save their information
+    '''
 
 	users_list = []
 	def __init__(self,first_name,last_name,password):
