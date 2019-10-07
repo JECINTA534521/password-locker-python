21cdb8d6ac621c020743395a36ed4fede9e3d3b3
jecinta <jecintawanjirug@gmail.com>
2019-10-07 12:06:01 +0300

    this adds a function that collects all the user details

--- password_locker.py
+++ password_locker.py
@@ -2,12 +2,14 @@
 import pyperclip
 from user_credentials import User, Credential
 
-def create_user(fname,lname,password):
+
+def create_user(fname, lname, password):
 	'''
 	Function to create a new user account
 	'''
-	new_user = User(fname,lname,password)
+	new_user = User(fname, lname, password)
 	return new_user
+
 
 def save_user(user):
     '''
@@ -16,12 +18,13 @@ def save_user(user):
     User.save_user(user)
 
 
-def verify_user(first_name,password):
+def verify_user(first_name, password):
     '''
     Function that verifies the existance of the user before creating the credentials
     '''
-    checking_user = Credential.check_user(first_name,password)
+    checking_user = Credential.check_user(first_name, password)
     return checking_user
+
 
 def generate_password():
 	'''
@@ -30,34 +33,40 @@ def generate_password():
 	gen_pass = Credential.generate_password()
 	return gen_pass
 
-def create_credential(user_name,site_name,account_name,password):
+
+def create_credential(user_name, site_name, account_name, password):
 	'''
 	Function to create a new credential
 	'''
-	new_credential=Credential(user_name,site_name,account_name,password)
+	new_credential = Credential(user_name, site_name, account_name, password)
 	return new_credential
+
 
 def save_credential(credential):
 	'''
 	Function to save a newly created credential
 	'''
 	Credential.save_credentials(credential)
+
+
 def display_credentials(user_name):
     '''
     Function to display credentials saved by a user
     '''
     return Credential.display_credentials(user_name)
-	
+
+
 def copy_credential(site_name):
 	'''
 	Function to copy the credentials details to the clipboard
 	'''
 	return Credential.copy_credential(site_name)
 
+
 def main():
-    print(' ')
-    print('Hi! Welcome to the Password Locker Python app')
-    while True:
+	print(' ')
+	print('Hi! Welcome to Password Locker.')
+	while True:
 		print(' ')
 		print("-"*60)
 		print('Use these codes to select: \n ca-Create an Account \n li-Log In \n ex-Exit')
